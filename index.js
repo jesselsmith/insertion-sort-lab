b6e696e11623245bd70e4d372bbe55b096b72ccb
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 13:49:59 -0700

    solved

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+    let min = array[0];
+    let minIndex = 0
+    array.slice(1).forEach((element, i) => {
+        if(element < min){
+            min = element;
+            minIndex = i + 1;
+        }
+    })
+    array.splice(minIndex, 1);
+    return min;
 }
 
 function selectionSort(array){
-
+    let sorted = []
+    let iterations = array.length
+    for(let i = 0; i < iterations; i++) {
+        sorted.push(findMinAndRemove(array))
+    }
+        
+    return sorted
 }
